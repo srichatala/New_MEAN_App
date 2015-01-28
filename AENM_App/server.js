2287d3b96ec4fb4d4c2976b3646ba45bbaa16e76
srichatala <sri2vas@gmail.com>
2015-01-28 15:41:48 -0800

    Mean App through Visual Studio

---
+++ AENM_App/server.js
@@ -0,0 +1,16 @@
+﻿var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var port = process.env.port || 1337;
+
+var app = express();
+app.use(bodyParser());
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (request, response) {
+    response.sendfile('index.html');
+});
+app.listen(port);
