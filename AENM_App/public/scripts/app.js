d544fba858cbaa0a48a80ab5e3830b15dd430723
srichatala <sri2vas@gmail.com>
2015-02-02 16:15:23 -0800

    app

---
+++ AENM_App/public/scripts/app.js
@@ -0,0 +1,44 @@
+﻿function serverClients($scope, $http) {
+    
+    //Get value from front-end
+    $scope.create = function () {
+        $http.post('/serviceClients', $scope.serverClient)
+        .success(function (response) {
+            $scope.all_info();
+            $scope.successTextAlert = "Patient record inserted";
+        });
+    }
+    
+    $scope.renderserviceClients = function (response) {
+        $scope.serviceClients = response;
+    };
+    
+    $scope.delete = function (id) {
+        $http.delete('/serviceClients/' + id)
+        .success(function (response) {
+            $scope.all_info();
+        });
+    };
+    
+    $scope.select = function (id) {
+        $http.get('/serviceClients/' + id)
+        .success(function (response) {
+            $scope.serverClient = response;
+        });
+    };
+    
+    $scope.update = function () {
+        $http.put('/serviceClients/' + $scope.serverClient._id, $scope.serverClient)
+        .success(function (response) {
+            $scope.all_info();
+            $scope.successTextAlert = "Patient record updated";
+        });
+    };
+    
+    $scope.all_info = function () {
+        $http.get('/serviceClients')
+        .success($scope.renderserviceClients);
+    }
+    
+    $scope.all_info();
+}
