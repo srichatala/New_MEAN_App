8b983783618a5b97522c724b19bebc4f63d8078e
srichatala <sri2vas@gmail.com>
2015-02-02 16:13:09 -0800

    full mean app

--- AENM_App/server.js
+++ AENM_App/server.js
@@ -1,16 +1,73 @@
-﻿var http = require('http');
-var express = require('express');
+﻿var express = require('express');
+var mongojs = require('mongojs');
 var bodyParser = require('body-parser');
-var path = require('path');
+var db = mongojs("mean", ["serviceClients"]);
+var app = express();
 
 var port = process.env.port || 1337;
 
-var app = express();
-app.use(bodyParser());
+app.use(bodyParser.json());
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+
+
+// to get the data to font-end
+app.get('/serviceClients', function (req, res) {
+    db.serviceClients.find(function (err, docs) {
+        res.json(docs);
+    });
+});
+
+//to delete data in database
+app.delete('/serviceClients/:id', function (req, res) {
+    var id = req.params.id;
+    db.serviceClients.remove({ _id: mongojs.ObjectId(id) }, function (err, doc) {
+        res.json(doc);
+    });
+});
+
+//to get single record
+app.get('/serviceClients/:id', function (req, res) {
+    var id = req.params.id;
+    db.serviceClients.findOne({ _id: mongojs.ObjectId(id) }, function (err, doc) {
+        res.json(doc);
+    });
+});
+
+app.put('/serviceClients/:id', function (req, res) {
+    var id = req.params.id;
+    db.serviceClients.findAndModify(
+        {
+            query: {
+                _id: mongojs.ObjectId(id)
+            },
+            update: {
+                _id: mongojs.ObjectId(id),
+                firstname: req.body.firstname,
+                lastname: req.body.lastname,
+                phoneno: req.body.phoneno, 
+                lvdate: req.body.lvdate, 
+                status: req.body.status
+            },
+            upsert: true
+        },
+        function (err, doc) {
+            res.json(doc);
+        });
+});
+
+//to post data from front-end
+app.post('/serviceClients', function (req, res) {
+    db.serviceClients.insert(req.body, function (err, doc) {
+        res.json(doc);
+    });
+});
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (request, response) {
-    response.sendfile('index.html');
+    response.sendFile("public/index.html");
 });
+
+
+
 app.listen(port);
